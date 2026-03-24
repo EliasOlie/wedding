19e0ece7b9d27f06536009ea289963bcda90135a
EliasOlie <eliasvieira31@gmail.com>
2026-03-24 14:10:47 -0300

    add: nivaldo

--- app/api/add/route.ts
+++ app/api/add/route.ts
@@ -10,53 +10,11 @@ export async function GET() {
     // 1. APENAS OS NOVOS CONVITES
     const novosConvites = [
       {
-        slug: "pr-carlos-e-familia",
-        saudacao: "Pr. Carlos e Família",
+        slug: "nivaldo-e-lena",
+        saudacao: "Presb. Nivaldo",
         convidados: [
-          { _id: "guest-79", name: "Pr. Carlos", status: "pending" },
-          { _id: "guest-80", name: "Cicera", status: "pending" },
-          { _id: "guest-81", name: "Aninha", status: "pending" },
-          { _id: "guest-82", name: "Elias", status: "pending" },
-        ],
-      },
-      {
-        slug: "pr-rogerio-e-paula",
-        saudacao: "Pr. Rogério e Paula",
-        convidados: [
-          { _id: "guest-83", name: "Pr. Rogério", status: "pending" },
-          { _id: "guest-84", name: "Paula", status: "pending" },
-        ],
-      },
-      {
-        slug: "reginaldo-e-mariah",
-        saudacao: "Presbítero Reginaldo e Mariah",
-        convidados: [
-          { _id: "guest-85", name: "Reginaldo", status: "pending" },
-          { _id: "guest-86", name: "Mariah", status: "pending" },
-        ],
-      },
-      {
-        slug: "toinho-e-maria-rosa",
-        saudacao: "Presbítero Toinho e Maria Rosa",
-        convidados: [
-          { _id: "guest-87", name: "Toinho", status: "pending" },
-          { _id: "guest-88", name: "Maria Rosa", status: "pending" },
-        ],
-      },
-      {
-        slug: "pedro-e-jessica",
-        saudacao: "Presbítero Pedro e Jéssica",
-        convidados: [
-          { _id: "guest-89", name: "Pedro", status: "pending" },
-          { _id: "guest-90", name: "Jéssica", status: "pending" },
-        ],
-      },
-      {
-        slug: "pr-irineu-e-esposa",
-        saudacao: "Pr. Irineu e Esposa",
-        convidados: [
-          { _id: "guest-91", name: "Pr. Irineu", status: "pending" },
-          { _id: "guest-92", name: "Esposa", status: "pending" },
+          { _id: "guest-91", name: "Presb. Nivaldo", status: "pending" },
+          { _id: "guest-92", name: "Lena", status: "pending" },
         ],
       },
     ];
